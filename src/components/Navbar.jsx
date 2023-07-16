53a0764cedbf3dff6ea68268ddb3c22eb16d0798
Himanshu Rajput <hiowner00@gmail.com>
2023-07-16 21:44:54 +0530

    responsiveness added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, Navigate, redirect } from "react-router-dom";
 import { context } from "../main";
 import axios from "axios";
 import { server } from "../main";
 import { toast } from "react-hot-toast";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  const {isAuthenticated, setIsAuthenticated, loading, setLoading} = useContext(context);
+  const {isAuthenticated, setIsAuthenticated, loading, setLoading, nav,setNav} = useContext(context);
+  const handleNav = () => {
+    setNav(!nav);
+  }
+
+  if(!nav){
+    document.body.style.overflow = "hidden";
+  }
+  else{
+    document.body.style.overflow = "visible"
+  }
   const logout = async () => {
     setLoading(true)
     try {
@@ -32,7 +43,7 @@ const Navbar = () => {
 
   return (
     <div className="w-full fixed h-[75px] bg-[#001C30] text-[#DAFFFB] flex items-center justify-around ">
-      <div className="w-[1250px] flex p-4 justify-between items-center">
+      <div className="w-[1250px] md:flex p-4 justify-between items-center hidden">
       <Link to='/'>
         <h1 className="uppercase text-4xl font-semibold">todo</h1>
       </Link>
@@ -73,6 +84,65 @@ const Navbar = () => {
           }
         </div>
       </div>
+          {/* <div className="flex w-full px-6 justify-between md:hidden">
+          <Link to='/'>
+        <h1 className="uppercase text-3xl font-semibold py-4">todo</h1>
+      </Link>
+      {
+        isAuthenticated ?
+        <div className="py-5">
+        <AiOutlineMenu size={30} className=""/>
+        </div> 
+        :
+        <Link
+            to="/login"
+            className="px-8 h-[40px] my-4 rounded-lg  flex items-center bg-[#176B87] font-semibold  hover:font-bold"
+          >
+            Howdy, user!
+          </Link>
+
+      }
+          </div> */}
+
+          <div className="flex p-4 justify-between md:hidden w-full bg-[#001C30] text-[#DAFFFB] h-[55px] items-center">
+      <div>
+      <Link to='/'>
+        <h1 className="uppercase text-3xl font-semibold py-4">todo</h1>
+      </Link>
+        </div>
+          {isAuthenticated ? 
+
+        <div onClick={handleNav} className="text-white">
+        {nav? <AiOutlineMenu size={25} className="font-extrabold" /> : <AiOutlineClose size={25} className="z-20"/> }
+        <div className={nav? 'absolute right-[-100%]' : 'fixed top-16 right-0 flex flex-col bg-[#001C30] text-[#DAFFFB]  w-full sm:w-[447px] p-4 h-[100%] py-3 ease-in-out duration-300 z-30'}>
+            <ul>
+              <li className=' text-4xl font-md px-4'><Link to='/'>Home</Link></li>
+              <li className=' text-4xl font-md px-4 py-5'><Link to='/profile'>Profile</Link></li>
+
+              <div className= 'border-t-2 border-white  w-5 my-10 mx-4'></div>
+              <li className='  text-3xl font-thin px-4 '> <button onClick={logout} disabled={loading}>Logout</button></li>
+            </ul>
+            <div className='flex  items-center mt-64 mx-4 cursor-pointer'>
+
+            <Link to='/'>
+        <h1 className="uppercase text-xl py-4">todo</h1>
+      </Link>
+            </div>
+          </div>
+        </div>
+        :
+        <div className="">
+        <Link
+            to="/login"
+            className="px-8 h-[40px] my-4 rounded-lg  flex items-center bg-[#176B87] font-semibold  hover:font-bold"
+          >
+            Howdy, user!
+          </Link>
+        </div>
+          }
+
+
+    </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   const [title, setTitle] = useState('')
   const [description ,  setDescription] = useState('')
   const [Loading, setLoading] = useState(false)
-  const {isAuthenticated} = useContext(context)
+  const {isAuthenticated, nav} = useContext(context)
   const [refresh, setRefresh] = useState(false)
 
   if(!isAuthenticated){
@@ -39,16 +39,19 @@ const Home = () => {
         }
     }
   return (
-    <div className='w-full h-screen bg-[#DAFFFB] flex justify-evenly '>
-      <div className=" flex flex-col items-center mt-24  ">
-      <form onSubmit={handleSubmit} className='flex flex-col ' >
-        <input type="text" name="task" id="task" className='lg:w-[450px] mt-12 rounded-md h-12 bg-[#001C30] outline-none text-[#DAFFFB] p-4 placeholder:text-[#DAFFFB] shadow-lg' placeholder='Task Title' value={title} onChange={(e) => setTitle(e.target.value)}  required/>
-        <textarea name="task" id="task" className='w-[450px] mt-4 rounded-md h-48 bg-[#001C30] outline-none text-[#DAFFFB] p-4 placeholder:text-[#DAFFFB] shadow-lg  ' placeholder='Task Description' value={description} onChange={(e) => setDescription(e.target.value)} required/>
+    <div className=' w-full min-h-screen h-full bg-[#DAFFFB] flex flex-col lg:flex-row  justify-evenly '>
+      {nav ? 
+      <div className=" flex flex-col items-center lg:mt-24 lg:p-0 fixed  top-16 lg:top-0 lg:left-[10%] pt-6 lg:max-w-fit lg:pt-0  w-screen bg-[#DAFFFB] pb-6">
+      
+      <form onSubmit={handleSubmit} className='flex flex-col items-center ' >
+        <input type="text" name="task" id="task" className='lg:w-[450px] w-[350px] mt-4 lg:mt-12 rounded-md h-12 bg-[#001C30] outline-none text-[#DAFFFB] p-4 placeholder:text-[#DAFFFB] shadow-lg' placeholder='Task Title' value={title} onChange={(e) => setTitle(e.target.value)}  required/>
+        <textarea name="task" id="task" className='lg:w-[450px] w-[350px] mt-4 rounded-md lg:h-48 bg-[#001C30] outline-none text-[#DAFFFB] p-4 placeholder:text-[#DAFFFB] shadow-lg  ' placeholder='Task Description' value={description} onChange={(e) => setDescription(e.target.value)} required/>
         <button type='submit' className='bg-[#001C30] text-[#DAFFFB] p-3 mt-6 px-8 rounded-md shadow-md block w-full font-semibold' disabled={Loading} >Add Your Task</button>
       </form>
-
+      <h1 className="text-5xl mt-8 font-bold text-[#001C30] lg:hidden">Tasks</h1>
       </div>
-      <div className="mt-32">
+      : <div></div>}
+      <div className="lg:mt-32 mt-[400px] lg:ml-[500px] pt-4 lg:pt-0 ">
         <Tasks refresh={refresh} setRefresh={setRefresh} />
       </div>
     </div>
